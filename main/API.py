91afaf8fce311c2e713fa394ae871b48ef9e9baf
MaxSoneback <maxsoneback@hotmail.com>
2019-10-11 14:26:20 +0800

    Added API.py. To test this program for your self, you need to acquire a key of your own

---
+++ main/API.py
@@ -0,0 +1,2 @@
+yandex_key = "MY_API_KEY" # This wont work obviously, but the API is free to use so follow the link in the README-file to get your own
+yandex_URI = f"https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key={yandex_key}"
